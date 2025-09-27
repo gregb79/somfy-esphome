1db27a87d7a61df6e8306ef2658b11aaf2f635ab
gregb79 <gregbassett@bigpond.com>
2025-09-27 15:28:08 +1000

    Update and rename somfy_binary_sensor.cpp to iris_binary_sensor.cpp

--- components/somfy/binary_sensor/iris_binary_sensor.cpp
+++ components/somfy/binary_sensor/iris_binary_sensor.cpp
@@ -1,28 +1,28 @@
 #include "esphome/core/log.h"
-#include "somfy_binary_sensor.h"
+#include "iris_binary_sensor.h"
 
 namespace esphome {
-namespace somfy {
+namespace iris {
 
-static const char *const TAG = "somfy";
+static const char *const TAG = "iris";
 
-void SomfyBinarySensor::update_windy(uint32_t address, bool value) {
+void IrisBinarySensor::update_windy(uint32_t address, bool value) {
   if (this->windy_binary_sensor_ && this->address_ == address) {
     this->windy_binary_sensor_->publish_state(value);
   }
 }
 
-void SomfyBinarySensor::update_sunny(uint32_t address, bool value) {
+void IrisBinarySensor::update_sunny(uint32_t address, bool value) {
   if (this->sunny_binary_sensor_ && this->address_ == address) {
     this->sunny_binary_sensor_->publish_state(value);
   }
 }
 
-void SomfyBinarySensor::dump_config() {
-  ESP_LOGCONFIG(TAG, "Somfy Binary Sensor: 0x%" PRIx32, this->address_);
+void IrisBinarySensor::dump_config() {
+  ESP_LOGCONFIG(TAG, "Iris Binary Sensor: 0x%" PRIx32, this->address_);
   LOG_BINARY_SENSOR("  ", "Windy", this->windy_binary_sensor_);
   LOG_BINARY_SENSOR("  ", "Sunny", this->sunny_binary_sensor_);
 }
 
-}  // namespace somfy
+}  // namespace iris
 }  // namespace esphome
