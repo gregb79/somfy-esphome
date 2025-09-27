59f72c5e1d72c08a1e756f6b46ac07e9dac6874b
gregb79 <gregbassett@bigpond.com>
2025-09-27 15:22:28 +1000

    Update automation.h

--- components/somfy/automation.h
+++ components/somfy/automation.h
@@ -2,38 +2,38 @@
 
 #include "esphome/core/component.h"
 #include "esphome/core/automation.h"
-#include "esphome/components/somfy/somfy.h"
+#include "esphome/components/iris/iris.h"
 
 namespace esphome {
-namespace somfy {
+namespace iris {
 
-template<typename... Ts> class SomfySetCodeAction : public Action<Ts...> {
+template<typename... Ts> class IrisSetCodeAction : public Action<Ts...> {
  public:
-  SomfySetCodeAction(SomfyComponent *somfy) : somfy_(somfy) {}
+  IrisSetCodeAction(IrisComponent *iris) : iris_(iris) {}
   TEMPLATABLE_VALUE(uint16_t, code)
 
   void play(Ts... x) override {
-    this->somfy_->set_code(this->code_.value(x...));
+    this->iris_->set_code(this->code_.value(x...));
   }
 
  protected:
-  SomfyComponent *somfy_;
+  IrisComponent *iris_;
 };
 
-template<typename... Ts> class SomfySendCommandAction : public Action<Ts...> {
+template<typename... Ts> class IrisSendCommandAction : public Action<Ts...> {
  public:
-  SomfySendCommandAction(SomfyComponent *somfy) : somfy_(somfy) {}
-  TEMPLATABLE_VALUE(SomfyCommand, command)
-  TEMPLATABLE_VALUE(SomfyMode, mode)
+  IrisSendCommandAction(IrisComponent *iris) : iris_(iris) {}
+  TEMPLATABLE_VALUE(IrisCommand, command)
+  TEMPLATABLE_VALUE(IrisMode, mode)
   TEMPLATABLE_VALUE(uint32_t, repeat)
 
   void play(Ts... x) override {
-    this->somfy_->send_command(this->command_.value(x...), this->mode_.value(x...), this->repeat_.value(x...));
+    this->iris_->send_command(this->command_.value(x...), this->mode_.value(x...), this->repeat_.value(x...));
   }
 
  protected:
-  SomfyComponent *somfy_;
+  IrisComponent *iris_;
 };
 
-}  // namespace somfy
+}  // namespace iris
 }  // namespace esphome
